520615cd978ee17ce63d18daad574137eedbcd20
Samet Kamgul <sametkamgul@gmail.com>
2020-12-24 18:52:06 +0300

    first commit

---
+++ resources/views/test_page.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>This is a test_page</h1>
+    <div>This is a test text-1</div>
+    <div>This is a test text-2</div>
+</body>
+</html>
